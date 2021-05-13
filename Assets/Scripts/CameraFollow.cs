7d04675ecd600ebe6d56e9b6b47d01618e74a582
Jack Coggins <voidawake90@gmail.com>
2021-05-13 23:40:16 +1000

    Polish menu scene

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,19 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float FollowSpeed = 2f;
-    public float yOffset = 1f;
-    public Transform target;
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private float followSpeed = 2f;
+    [SerializeField] private float yOffset = 1f;
+    [SerializeField] private Transform target;
+    
+    private void Update()
     {
         Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
-        transform.position = Vector3.Slerp(transform.position, newPos,FollowSpeed);
+        transform.position = Vector3.Lerp(transform.position, newPos, followSpeed * Time.deltaTime);
     }
-
-
-
 }
