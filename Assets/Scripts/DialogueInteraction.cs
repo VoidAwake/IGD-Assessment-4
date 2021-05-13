0f869f91f31c63ccdfca8092bf0727fe846432ad
Jack Coggins <voidawake90@gmail.com>
2021-05-13 15:20:36 +1000

    Rename ItemInteraction to DialogueInteraction

--- Assets/Scripts/DialogueInteraction.cs
+++ Assets/Scripts/DialogueInteraction.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemInteraction : Interactable
+public class DialogueInteraction : Interactable
 {
 	/*README---
 	 * 
