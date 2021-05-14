76fea485e883d7fad82f44ad44013c1b764d1bd9
Jack Coggins <voidawake90@gmail.com>
2021-05-14 11:48:35 +1000

    Start room one layout

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private float followSpeed = 2f;
-    [SerializeField] private float yOffset = 1f;
     [SerializeField] private Transform target;
+
+    private Vector3 basePosition;
+
+    private void Start() {
+        basePosition = transform.position;
+    }
     
     private void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+        Vector3 newPos = new Vector3(target.position.x, basePosition.y, basePosition.z);
         transform.position = Vector3.Lerp(transform.position, newPos, followSpeed * Time.deltaTime);
     }
 }
