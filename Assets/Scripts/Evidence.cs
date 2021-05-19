112e505a9c9e42889a1065e187135de5f4eba78d
Jack Coggins <voidawake90@gmail.com>
2021-05-19 20:26:07 +1000

    Remove description field  from Evidence

--- Assets/Scripts/Evidence.cs
+++ Assets/Scripts/Evidence.cs
@@ -6,7 +6,6 @@ namespace DefaultNamespace
     [CreateAssetMenu]
     public class Evidence : ScriptableObject
     {
-        public List<string> descriptions;
         public List<EvidencePiece> evidencePieces;
         [TextArea] public string dialogue;
         public Sprite speakerSprite;
