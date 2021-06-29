082d8ba4168d5e0d4f5ae8deb8060d991c7adc14
Jack Coggins <voidawake90@gmail.com>
2021-06-29 17:28:23 +1000

    Refactor lock picking

--- Assets/Scripts/Room 1/LockPick.cs
+++ Assets/Scripts/Room 1/LockPick.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,94 +5,88 @@ public class LockPick : MonoBehaviour
 {
     [SerializeField] private int nextSceneBuildIndex;
 
-    public Camera cam;
-    public Transform innerLock;
-    public Transform pickPosition;
+    [SerializeField] private Camera cam;
+    [SerializeField] private Transform innerLock;
+    [SerializeField] private Transform pickPosition;
 
-    public float maxAngle = 90;
-    public float lockSpeed = 5;
+    [SerializeField] private float maxAngle = 90;
+    [SerializeField] private float lockSpeed = 5;
 
     [Range(1,25)]
-    public float lockRange = 10;
+    [SerializeField] private float lockRange = 10;
 
     private float eulerAngle;
     private float unlockAngle;
-    private Vector2 unlockRange;
 
-    private float keyPressTime = 0;
+    private int KeyPressTime => Input.GetMouseButton(0) ? 1 : 0;
 
-    private bool movePick;
-    // Start is called before the first frame update
     void Start()
     {
-        newLock();
+	    // Ensure the unlocking angle is larger than the lock range.
+	    unlockAngle = Random.Range(-maxAngle + lockRange, maxAngle - lockRange);
+	    
+	    transform.localPosition = pickPosition.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.localPosition = pickPosition.position;
+	    if (!Input.GetMouseButton(0))
+		    RotatePick();
 
-		if (movePick)
-		{
-            Vector3 dir = Input.mousePosition -cam.WorldToScreenPoint(transform.position);
+	    RotateLock();
 
-            eulerAngle = Vector3.Angle(dir, new Vector3(0,1,0));
-
-            Vector3 cross = Vector3.Cross(Vector3.up, dir);
-            if (cross.z <= 0)
-			{
-                eulerAngle = -eulerAngle;
-			}
-
-            eulerAngle = Mathf.Clamp(eulerAngle, -maxAngle, maxAngle);
-
-            Quaternion rotateTo = Quaternion.AngleAxis(eulerAngle, Vector3.forward);
-            transform.rotation = rotateTo;
-		}
-        if (Input.GetMouseButtonDown(0))
-		{
-            movePick = false;
-            keyPressTime = 1;
-		}
-        if (Input.GetMouseButtonUp(0))
-		{
-            movePick = true;
-            keyPressTime = 0;
-		}
-
-        //The Turn angle of the inner lock. If it matches the 90 degrees.
-		float percentage = Mathf.Round(100 - Mathf.Abs(((eulerAngle - unlockAngle) / 100) * 100));
-        float lockRotation = ((percentage / 100) * maxAngle) * keyPressTime;
-        float maxRotation = (percentage / 100) * maxAngle;  
-
-        //returns a float range of the min and max, with lockspeed * time.deltatime being the increments.
-        float lockLerp = Mathf.Lerp(innerLock.eulerAngles.z, lockRotation, lockSpeed * Time.deltaTime);
-        innerLock.eulerAngles = new Vector3(0, 0, lockLerp);
-
-        if (lockLerp >= maxRotation - 1)
-		{
-            if (eulerAngle < unlockRange.y && eulerAngle > unlockRange.x)
-			{
-                //Does something once it unlocks.
-                Debug.Log("It Unlocks");
-                SceneManager.LoadScene(nextSceneBuildIndex);
-                movePick = true;
-                keyPressTime = 0;
-			}
-			else
-			{
-                //The Pin will Shake if incorrect.
-                float randomRotation = Random.insideUnitCircle.x;
-                transform.eulerAngles += new Vector3(0, 0, Random.Range(-randomRotation, randomRotation));
-			}
-		}
+	    if (Input.GetMouseButtonDown(0))
+		    Debug.Log(eulerAngle);
     }
 
-    void newLock()
-	{
-        //Ensure the unlocking angle is larger than the lock range.
-        unlockAngle = Random.Range(-maxAngle + lockRange, maxAngle - lockRange);
-        unlockRange = new Vector2(unlockAngle - lockRange, unlockAngle + lockRange);
+    private void RotatePick()
+    {
+	    Vector3 dir = Input.mousePosition -cam.WorldToScreenPoint(transform.position);
+
+	    eulerAngle = Vector3.Angle(dir, new Vector3(0,1,0));
+
+	    Vector3 cross = Vector3.Cross(Vector3.up, dir);
+	    if (cross.z <= 0)
+	    {
+		    eulerAngle = -eulerAngle;
+	    }
+
+	    eulerAngle = Mathf.Clamp(eulerAngle, -maxAngle, maxAngle);
+
+	    Quaternion rotateTo = Quaternion.AngleAxis(eulerAngle, Vector3.forward);
+	    transform.rotation = rotateTo;
+    }
+
+    private void RotateLock()
+    {
+	    float angleDifference = Mathf.Abs(eulerAngle - unlockAngle);
+	    float normalisedAngleDifference = angleDifference / 180;
+	    float percentage = (1 - normalisedAngleDifference) * 100;
+	    float lockRotation = percentage / 100 * maxAngle * KeyPressTime;
+	    float maxRotation = percentage / 100 * maxAngle;
+
+	    //returns a float range of the min and max, with lockspeed * time.deltatime being the increments.
+	    float lockLerp = Mathf.Lerp(innerLock.eulerAngles.z, lockRotation, lockSpeed * Time.deltaTime);
+	    innerLock.eulerAngles = new Vector3(0, 0, lockLerp);
+
+	    if (lockLerp >= maxRotation - 1)
+	    {
+		    if (WithinUnlockRange())
+		    {
+			    // Does something once it unlocks.
+			    SceneManager.LoadScene(nextSceneBuildIndex);
+		    }
+		    else
+		    {
+			    // The pick will shake if incorrect.
+			    float randomRotation = Random.insideUnitCircle.x;
+			    transform.eulerAngles += new Vector3(0, 0, Random.Range(-randomRotation, randomRotation));
+		    }
+	    }
+    }
+
+    private bool WithinUnlockRange()
+    {
+	    return eulerAngle < unlockAngle + lockRange && eulerAngle > unlockAngle - lockRange;
     }
 }
